250fdb8a07027ff7c7529901baeb29ba0be6de6b
Gjovani Gorvokaj <ggorvokaj@gmail.com>
2025-01-28 20:16:17 -0500

    updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require("dotenv").config({ path: "./.env" });
 
 const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-console.log("Using shared secrets from .env file:");
-console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
-console.log(`SECRET_KEY: ${process.env.SECRET_KEY}`);
+console.log("✅ Using shared secrets from .env file:");
+console.log(`🌍 NODE_ENV: ${process.env.NODE_ENV}`);
+console.log(`🔑 SECRET_KEY: ${process.env.SECRET_KEY}`);
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
@@ -15,5 +15,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`🚀 Server running at http://${hostname}:${port}/`);
 });
